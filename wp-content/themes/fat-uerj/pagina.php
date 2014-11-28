4a48325421d0b9c78ebc52ad105877e46ae2a1d7
alexandre <alexandre@notebook-alexandre>
2014-11-27 23:02:20 -0200

    Adicionando template "Página" para uso nas páginas do site.

---
+++ wp-content/themes/fat-uerj/pagina.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* Template Name: Página 
+* @package WordPress
+* @subpackage fat-uerj
+* @since FAT-UERJ 1.0
+*/
+get_header(); ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron internal">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+              <h3 class="text-uppercase"><?php the_field("tipo_pagina") ?></h3>
+            <ul class="breadcrumb">
+              <li><a href="#">Home</a></li>
+              <li><a href="#"><?php the_field("tipo_pagina") ?></a></li>
+              <li class="active"><?php the_title() ?></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container main internal">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="text-uppercase page-header"><?php the_title() ?></h2>
+              <p><?php echo get_the_post_thumbnail(get_the_ID(), 'featured-blog-thumb', array('class'=>'thumbnail')); ?></p>
+              <?php the_content() ?>
+            </div>
+          </div>
+        </div>
+        <aside class="col-md-3">
+          <ul class="list-group">
+            <a href="#" class="list-group-item active">Sala de aula</a>
+            <a href="#" class="list-group-item">Laboratórios de física</a>
+            <a href="#" class="list-group-item">Laboratórios de informática</a>
+            <a href="#" class="list-group-item">Secretarias</a>
+            <a href="#" class="list-group-item">Sala de multimídia</a>
+            <a href="#" class="list-group-item">Espaço Kodak</a>
+            <a href="#" class="list-group-item">Refeitório</a>
+            <a href="#" class="list-group-item">Sala de Reprografia</a>
+            <a href="#" class="list-group-item">Administração</a>
+          </ul>
+        </aside>
+      </div>
+    </div>
+<?php endwhile; ?>
+<?php get_footer(); ?>
