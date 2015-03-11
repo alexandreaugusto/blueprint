69885803bee5203180e2b8c0714f91ca83c1cc0e
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2015-03-11 18:03:17 -0300

    Arrumando link errado dos CSS.

--- wp-content/themes/fat-uerj/header.php
+++ wp-content/themes/fat-uerj/header.php
@@ -13,11 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo('name'); ?></title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom -->
-    <link href="css/custom.css" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -44,7 +40,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img src="img/logo.png" alt="Faculdade de Tecnologia UERJ - Resende"></a>
+          <a class="navbar-brand" href="#"><img src="<?php bloginfo('template_url'); ?>/img/logo-uerj.png" alt="Faculdade de Tecnologia UERJ - Resende"></a>
           <div class="pull-right share hidden-xs">
             <span class='st_facebook_large' displayText='Facebook'></span>
             <span class='st_twitter_large' displayText='Tweet'></span>
