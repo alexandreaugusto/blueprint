ad930738ad8ada244b723f7d3ba84874f6b8f3f5
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2015-03-12 16:37:20 -0300

    Acertando detalhes da page.php e separando sidebar.

--- wp-content/themes/fat-uerj/sidebar-interna-nav.php
+++ wp-content/themes/fat-uerj/sidebar-interna-nav.php
@@ -10,11 +10,7 @@
                 <aside class="col-md-3">
                     <ul class="list-group">
                         <?php while($related->have_posts()):$related->the_post(); ?>
-                        <?php if(get_the_title() == $title): ?>
-                        <a href="<?php the_permalink() ?>" class="list-group-item active"><?php the_title() ?></a>
-                        <?php else: ?>
                         <a href="<?php the_permalink() ?>" class="list-group-item"><?php the_title() ?></a>
-                        <?php endif; ?>
                         <?php endwhile; ?>
                     </ul>
                 </aside>
