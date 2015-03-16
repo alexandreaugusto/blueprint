a79765b866dcef13512cd64f07bc0b8c60ccfdb8
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2015-03-16 11:38:41 -0300

    Dinamizando a sidebar da página interna de departamentos.

--- wp-content/themes/fat-uerj/sidebar-departamento.php
+++ wp-content/themes/fat-uerj/sidebar-departamento.php
@@ -9,7 +9,7 @@
                 <ul class="list-group">
                     <?php $departamentos = new WP_Query(array('post_type' => 'page', 'category_name' => 'departamentos', 'orderby' => 'ID', 'order' => 'ASC')); ?>
                     <?php while ($departamentos->have_posts()):$departamentos->the_post(); ?>
-                    <li><a href="<?php the_permalink(); ?>" class="list-group-item active"><?php the_title(); ?></a></li>
+                    <a href="<?php the_permalink(); ?>" class="list-group-item"><?php the_title(); ?></a>
                     <?php endwhile; ?>
                 </ul>
             </aside>
