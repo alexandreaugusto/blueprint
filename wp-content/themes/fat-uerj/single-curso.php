d2a0d49aa6432998ec463de40d9b499f68db7f0d
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2015-03-18 17:25:38 -0300

    Colocando link da categoria listada no breadcrumb da página dos cursos.

--- wp-content/themes/fat-uerj/single-curso.php
+++ wp-content/themes/fat-uerj/single-curso.php
@@ -26,7 +26,7 @@ get_header(); ?>
               <li><a href="<?php bloginfo("url") ?>">Home</a></li>
               <li><a href="#">Ensino</a></li>
               <?php foreach($categories as $cat): ?>
-              <li><a href="<?php echo esc_url(get_category_link(get_cat_ID($cat->name))); ?>"><?php echo $cat->name; ?></a></li>
+              <li><a href="<?php echo esc_url(get_term_link($cat->slug)); ?>"><?php echo $cat->name; ?></a></li>
               <?php endforeach; ?>
               <li class="active"><?php the_title(); ?></li>
             </ul>
